f23ab6a6d149a41b9b755706a1f0ebee8ded31d2
angel3mivC <151053848+angel3mivC@users.noreply.github.com>
2024-03-30 19:39:53 -0600

    Add files via upload

--- line_follower.py
+++ line_follower.py
@@ -2,49 +2,101 @@ from djitellopy import tello
 import numpy as np
 import cv2
 
-def nothing(): pass
+def get_sensors(mask):
+    imgs = np.hsplit(mask, 3)
+    sensors = []
+    for i, img in enumerate(imgs):
+        pixelCount = cv2.countNonZero(img)
+        #11520 representa en 20% del los pixeles de la imagen
+        if pixelCount > 11520:
+            sensors.append(1)
+        else:
+            sensors.append(0)
+    return sensors
 
 cv2.namedWindow("Line")
-cv2.createTrackbar("L-H", "Line", 0, 180, nothing)
-cv2.createTrackbar("L-S", "Line", 121, 255, nothing)
-cv2.createTrackbar("L-V", "Line", 109, 255, nothing)
-cv2.createTrackbar("U-H", "Line", 180, 180, nothing)
-cv2.createTrackbar("U-S", "Line", 255, 255, nothing)
-cv2.createTrackbar("U-V", "Line", 243, 255, nothing)
+cv2.createTrackbar("L-H", "Line", 0, 180, lambda x:None)
+cv2.createTrackbar("L-S", "Line", 0, 255, lambda x:None)
+cv2.createTrackbar("L-V", "Line", 200, 255, lambda x:None)
+cv2.createTrackbar("U-H", "Line", 180, 180, lambda x:None)
+cv2.createTrackbar("U-S", "Line", 30, 255, lambda x:None)
+cv2.createTrackbar("U-V", "Line", 255, 255, lambda x:None)
+
+# threshold = 0.2
+# totalPixels = 57600
+# sensors = np.array(3, dtype=bool)
+
+# me = tello.Tello()
+# me.connect()
+# print(me.get_battery())
+# me.streamon()
+
+# me.takeoff()
 
 capture = cv2.VideoCapture(0)
 
+width = 480
+senstivity = 3
+fspeed = 15
+
 while True:
-    frame = capture.read()
+    l_h = cv2.getTrackbarPos("L-H", "Line")
+    l_s = cv2.getTrackbarPos("L-S", "Line")
+    l_v = cv2.getTrackbarPos("L-V", "Line")
+    u_h = cv2.getTrackbarPos("U-H", "Line")
+    u_s = cv2.getTrackbarPos("U-S", "Line")
+    u_v = cv2.getTrackbarPos("U-V", "Line")
+
+    _, frame = capture.read()
+    # frame = me.get_frame_read().frame
+    # frame = cv2.flip(frame, 0)
+    frame = cv2.resize(frame, (480, 360))
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower = np.array([l_h, l_s, l_v])
+    upper = np.array([u_h, u_s, u_v])
+    mask = cv2.inRange(hsv, lower, upper)
 
-    l_h = cv2.getTrackbarPos("L-H", "HSV Configuration")
-    l_s = cv2.getTrackbarPos("L-S", "HSV Configuration")
-    l_v = cv2.getTrackbarPos("L-V", "HSV Configuration")
-    u_h = cv2.getTrackbarPos("U-H", "HSV Configuration")
-    u_s = cv2.getTrackbarPos("U-S", "HSV Configuration")
-    u_v = cv2.getTrackbarPos("U-V", "HSV Configuration")
+    contours, _ =cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(frame, contours, -1, (255,0,0), 5)
 
-    lower_red = np.array([l_h, l_s, l_v])
-    upper_red = np.array([u_h, u_s, u_v])
+    if contours:
+        biggest = max(contours, key=cv2.contourArea)
+        x, y, w, h = cv2.boundingRect(biggest)
+        cx = x + w//2
+        cy = y + h//2
 
-    mask = cv2.inRange(hsv, lower_red, upper_red)
+        cv2.circle(frame, (cx, cy), 10, (255,0,0), cv2.FILLED)
 
-    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    biggest = max(contours, key = cv2.contourArea())
+    sensors = get_sensors(mask)
 
-    x, y, w, h = cv2.boundingRect(biggest)
-    cx = x + w//2
-    cy = y + h//2
+    #Envio de comandos
 
-    cv2.drawContours(frame, contours, -1, (0,0,255), 7)
-    cv2.circle(frame, (cx, cy), 10, (0,0,255), cv2.FILLED)
+    #Translation
+    lr = cx - width//2
+    lr = int(np.clip(lr, -10, 10))
+    if lr < 2 and lr > -2: lr = 0
 
-    
+    #Rotation
+
+    if sensors == [1, 0, 0]: curve = -25
+    elif sensors == [1, 1, 0]: curve = -15
+    elif sensors == [0, 1, 0]: curve = 0
+    elif sensors == [0, 1, 1]: curve = 15
+    elif sensors == [0, 0, 1]: curve = 25
+    elif sensors == [0, 0, 0]: curve = 0
+    elif sensors == [1, 1, 1]: curve = 0
+    elif sensors == [1, 0, 1]: curve = 0
+
+    # me.send_rc_control(lr, fspeed, 0, curve)
 
 
-    if cv2.waitKey(1) == 27: break
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Mask", mask)
+
+    if cv2.waitKey(1000) == 27: break
+
+# me.land()
 
 capture.release()
 cv2.destroyAllWindows()
--- shape_detection.py
+++ shape_detection.py
@@ -1,34 +1,36 @@
 import cv2
 import numpy as np
 
-def nothing(): pass
-
 cv2.namedWindow("HSV Configuration")
-cv2.createTrackbar("L-H", "HSV Configuration", 0, 255, nothing)
-cv2.createTrackbar("L-S", "HSV Configuration", 121, 255, nothing)
-cv2.createTrackbar("L-V", "HSV Configuration", 109, 255, nothing)
-cv2.createTrackbar("U-H", "HSV Configuration", 255, 255, nothing)
-cv2.createTrackbar("U-S", "HSV Configuration", 255, 255, nothing)
-cv2.createTrackbar("U-V", "HSV Configuration", 243, 255, nothing)
+cv2.createTrackbar("L-H", "HSV Configuration", 0, 255, lambda x:None)
+cv2.createTrackbar("L-S", "HSV Configuration", 121, 255, lambda x:None)
+cv2.createTrackbar("L-V", "HSV Configuration", 109, 255, lambda x:None)
+cv2.createTrackbar("U-H", "HSV Configuration", 255, 255, lambda x:None)
+cv2.createTrackbar("U-S", "HSV Configuration", 255, 255, lambda x:None)
+cv2.createTrackbar("U-V", "HSV Configuration", 243, 255, lambda x:None)
 
 capture = cv2.VideoCapture(0)
 font = cv2.FONT_HERSHEY_COMPLEX
 kernel = np.ones((5,5), np.uint8)
 
+l_h = cv2.getTrackbarPos("L-H", "HSV Configuration")
+l_s = cv2.getTrackbarPos("L-S", "HSV Configuration")
+l_v = cv2.getTrackbarPos("L-V", "HSV Configuration")
+u_h = cv2.getTrackbarPos("U-H", "HSV Configuration")
+u_s = cv2.getTrackbarPos("U-S", "HSV Configuration")
+u_v = cv2.getTrackbarPos("U-V", "HSV Configuration")
+
+lower_red = np.array([l_h, l_s, l_v])
+upper_red = np.array([u_h, u_s, u_v])
+
+vertex_x = np.empty(4, dtype=np.int16)
+vertex_y = np.empty(4, dtype=np.int16)
+aux = 0
+
 while True:
     _, frame = capture.read()
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    l_h = cv2.getTrackbarPos("L-H", "HSV Configuration")
-    l_s = cv2.getTrackbarPos("L-S", "HSV Configuration")
-    l_v = cv2.getTrackbarPos("L-V", "HSV Configuration")
-    u_h = cv2.getTrackbarPos("U-H", "HSV Configuration")
-    u_s = cv2.getTrackbarPos("U-S", "HSV Configuration")
-    u_v = cv2.getTrackbarPos("U-V", "HSV Configuration")
-
-    lower_red = np.array([l_h, l_s, l_v])
-    upper_red = np.array([u_h, u_s, u_v])
 
     mask = cv2.inRange(hsv, lower_red, upper_red)
     mask = cv2.erode(mask, kernel)
@@ -37,9 +39,9 @@ while True:
 
     for cntr in contours:
         area = cv2.contourArea(cntr)
-        approx = cv2.approxPolyDP(cntr, 0.02*cv2.arcLength(cntr, True), True)
 
         if area > 400:
+            approx = cv2.approxPolyDP(cntr, 0.03*cv2.arcLength(cntr, True), True)
             cv2.drawContours(frame, [approx], 0, (0,0,0), 2)
             
             x = approx.ravel()[0]
@@ -48,12 +50,46 @@ while True:
             if len(approx) == 3:
                 cv2.putText(frame, "Triangle", (x, y), font, 1, (0,0,0))
             elif len(approx) == 4:
-                cv2.putText(frame, "Rectangle", (x, y), font, 1, (0,0,0))
+
+                for i in range(4):
+                    vertex_x[i] = approx[i][0][0]
+                    vertex_y[i] = approx[i][0][1]
+
+                for i in range(4):
+                    for j in range(3):
+                        if vertex_x[j] > vertex_x[j+1]:
+                            aux = vertex_x[j+1]
+                            vertex_x[j+1] = vertex_x[j]
+                            vertex_x[j] = aux
+
+                            aux = vertex_y[j+1]
+                            vertex_y[j+1] = vertex_y[j]
+                            vertex_y[j] = aux
+
+                if vertex_y[0] > vertex_y[1]:
+                    aux = vertex_y[1]
+                    vertex_y[1] = vertex_y[0]
+                    vertex_y[0] = aux
+
+                if vertex_y[2] > vertex_y[3]:
+                    aux = vertex_y[3]
+                    vertex_y[3] = vertex_y[2]
+                    vertex_y[2] = aux
+                    
+                upper_difference = abs(vertex_y[0] - vertex_y[2])
+                lower_difference = abs(vertex_y[1] - vertex_y[3])
+
+                if upper_difference < 50 and lower_difference < 50:
+                    cv2.putText(frame, "Square", (x, y), font, 1, (0,0,0))
+                else:
+                    cv2.putText(frame, "Rhombus", (x, y), font, 1, (0,0,0))
+
+            elif len(approx) == 5:
+                cv2.putText(frame, "Pentagon", (x, y), font, 1, (0,0,0))
             elif len(approx) == 10:
                 cv2.putText(frame, "Star", (x, y), font, 1, (0,0,0))
             elif len(approx) > 10:
                 cv2.putText(frame, "Circle", (x, y), font, 1, (0,0,0))
-
 
     cv2.imshow("Capture", frame)
     cv2.imshow("Mask", mask)
