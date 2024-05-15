e6b98152ec9bb3a480273e0b160e896303b159ab
Otrebor280971 <roberto.castro.gomez.05@gmail.com>
2024-05-14 21:50:46 -0600

    Update basic_movement.py

--- basic_movement.py
+++ basic_movement.py
@@ -10,22 +10,26 @@ print(me.get_battery())
 #Basic movement
 me.streamon()
 
-me.takeoff()
-me.send_rc_control()
-me.send_rc_control(0, 5, 0, 0)
-sleep(5)
-me.send_rc_control(0, 0, 0, 0)
-
-#Image Capture
-print(me.stream_on)
 
 while True:
+
     frame = me.get_frame_read().frame
     cv2.imshow("Capture", frame)
-
     if cv2.waitKey(1) == 27:
         me.send_rc_control(0,0,0,0)
         me.land()
         break
+    
+me.takeoff()
+me.move_forward(100) 
+me.send_rc_control()
+me.send_rc_control(0, 100, 0, 0)
+sleep(2)
+me.send_rc_control(0, 0, 0, 0)
+me.land()
+
+#Image Capture
+print(me.stream_on)
+
 
 cv2.destroyAllWindows()
